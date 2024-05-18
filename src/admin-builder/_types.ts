2c8401902a3e2767a42e7cc91fdf0a2227692757
etherealelement <vitalik.safonov@list.ru>
2024-05-18 09:03:05 +0300

    add: builder config

--- src/admin-builder/_types.ts
+++ src/admin-builder/_types.ts
@@ -1,26 +1,25 @@
-type TextField = {
-    type: "text";
-    name: string;
-    title: string;
-    inputType: string;
-}
-
 type Options = {
-    label: string;
-    value: string;
-}
+	label: string;
+	value: string;
+};
+
+type TextField = {
+	type: 'text';
+	name: string;
+	title: string;
+};
 
 type SelectField = {
-    type: "select";
-    name: string;
-    title: string;
-    options: Options[];
-}
+	type: 'select';
+	name: string;
+	title: string;
+	options?: Options[];
+};
 
 export type AdminBuilderField = TextField | SelectField;
 
-export type AdminBuilderConfig = {
-    name: string;
-    title: string;
-    fields: AdminBuilderField;
-}
+export type AdminEntityConfig = {
+	name: string;
+	title: string;
+	fields: AdminBuilderField[];
+};
